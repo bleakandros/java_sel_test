f249b2876ef158e32bef82db15e75212d67b4967
bleakandros <73052773+bleakandros@users.noreply.github.com>
2020-10-25 16:54:35 +0500

    StarShip complited

--- first/src/main/java/FirstTest.java
+++ first/src/main/java/FirstTest.java
@@ -1,24 +1,15 @@
+import test.ship.StarShip;
+
 public class FirstTest {
 
     public static void main(String[] args) {
-        helloTest();
-        helloTest( "username");
-        System.out.println(helloTest("Значение 1", " Значение 2"));
+        //создаю экземпляр класса корабля
+        StarShip ship = new StarShip( 253.25);
+        displayHeight(ship);
     }
 
-    /*демо методов*/
-    //1 метод без параметров
-    public static void helloTest() {
-        System.out.println("We are hire! Hello!");
-
-    }
-    //2 метод с параметром
-    public static void helloTest (String value) {
-        System.out.println("We are hire! Hello! " + value);
-    }
-    //3 метод с возвратом значения
-    public static String helloTest (String value1, String value2) {
-              return "We are hire! Hello! " + value1 + "," + value2;
+    public static void displayHeight(StarShip ship) {
+        System.out.println("Высота корабля составляет: " + ship.getShipHeight());
     }
 
 
--- first/src/main/java/test/ship/StarShip.java
+++ first/src/main/java/test/ship/StarShip.java
@@ -5,4 +5,13 @@ public class StarShip {
     /*высота*/
     public double shipHeight;
 
+    //добавить конструктор для корабля
+    public StarShip(double shipHeight) {
+        this.shipHeight = shipHeight;
+    }
+        //возращает высоту корабля
+        public double getShipHeight() {
+            return this.shipHeight;
+        }
+
 }
