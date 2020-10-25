190b8ff3c8930b251596a4015efc2eb432858d24
bleakandros <73052773+bleakandros@users.noreply.github.com>
2020-10-25 13:45:52 +0500

    StarShip begin

--- first/src/main/java/FirstTest.java
+++ first/src/main/java/FirstTest.java
@@ -1,6 +1,25 @@
 public class FirstTest {
 
-	public static void main (String[] args) {
-		System.out.println ("We are here! Hello!");
-	}
+    public static void main(String[] args) {
+        helloTest();
+        helloTest( "username");
+        System.out.println(helloTest("Значение 1", " Значение 2"));
+    }
+
+    /*демо методов*/
+    //1 метод без параметров
+    public static void helloTest() {
+        System.out.println("We are hire! Hello!");
+
+    }
+    //2 метод с параметром
+    public static void helloTest (String value) {
+        System.out.println("We are hire! Hello! " + value);
+    }
+    //3 метод с возвратом значения
+    public static String helloTest (String value1, String value2) {
+              return "We are hire! Hello! " + value1 + "," + value2;
+    }
+
+
 }
---
+++ first/src/main/java/test/ship/StarShip.java
@@ -0,0 +1,8 @@
+package test.ship;
+/*Created by Andrei 10 2020*/
+
+public class StarShip {
+    /*высота*/
+    public double shipHeight;
+
+}
